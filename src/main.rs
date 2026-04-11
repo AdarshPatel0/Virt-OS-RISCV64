ac25c87bb618de4746c86ddd0ee22c9fb90f77c4
Adarsh <adarsh2005@outlook.com>
2026-04-11 07:05:29 +0000

    commit

--- src/main.rs
+++ src/main.rs
@@ -4,35 +4,37 @@
 mod device_tree_utils;
 mod panic_handler;
 mod system_utils;
-mod timer_interrupt;
-mod thread;
 
 use buddy_system_allocator::LockedHeap;
 
 unsafe extern "C" {
-    static kernel_end: u8;
+    static _kernel_end: u8;
 }
+
+core::arch::global_asm!(
+    ".section .text.entry",
+    ".globl _start",
+    "_start:",
+    "    la sp, _stack_top",
+    "    j kmain"
+);
 
 #[global_allocator]
 pub static HEAP: LockedHeap<32> = LockedHeap::empty();
 
-#[riscv_rt::entry]
-fn main(_hart_id: usize, device_tree_binary_ptr: usize) -> ! {
+#[unsafe(no_mangle)]
+pub extern "C" fn kmain(_hart_id: usize, device_tree_binary_ptr: usize) -> ! {
     let device_tree = device_tree_utils::read_device_tree_data(device_tree_binary_ptr).expect("Failed to read device tree.");
 
     let (system_memory_base_address, system_memory_amount) = device_tree_utils::get_system_memory_info(&device_tree).expect("Failed to get system memory amount.");
 
-    let kernel_end_address = core::ptr::addr_of!(kernel_end) as usize;
+    let kernel_end_address = core::ptr::addr_of!(_kernel_end) as usize;
 
     unsafe {
         HEAP.lock().add_to_heap(kernel_end_address, system_memory_base_address + system_memory_amount);
-    }
+    };
 
-    unsafe {
-        riscv::interrupt::supervisor::enable();
-    }
+    
 
-    timer_interrupt::init(1_000_000);
-
-    loop {}
+    system_utils::shutdown()
 }
--- src/thread.rs
+++
@@ -1,5 +0,0 @@
-struct Thread {
-    stack_pointer: *const u8,
-    stack_size: usize,
-    
-}
