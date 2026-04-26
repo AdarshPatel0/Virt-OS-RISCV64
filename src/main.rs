db96022d8bf332264acd7ec6c41df014e5657d12
Adarsh <adarsh2005@outlook.com>
2026-04-26 21:35:53 +0000

    commit

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 #![no_main]
 #![no_std]
 
+use crate::print::println;
+
 extern crate alloc;
 
 mod context;
@@ -15,7 +17,7 @@ mod thread;
 mod timer_interrupt;
 mod trap_handler;
 mod virtio_hal;
-mod disk;
+mod devices;
 
 unsafe extern "C" {
     static _kernel_end: u8;
@@ -53,7 +55,7 @@ extern "C" fn kmain(hart_id: usize, device_tree_binary_ptr: usize) -> ! {
         drop(heap);
     };
 
-    disk::load_disks(device_tree);
+    devices::load_drivers(device_tree);
 
     thread::create_thread(programs::shell::new as *const u8 as usize, false, 4096, &[]);
 
