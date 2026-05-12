fca6e6b4e4ea706178c6b0e296a4ae4730e2d164
Adarsh <adarsh2005@outlook.com>
2026-05-12 23:33:01 +0000

    commit

---
+++ src/ext4_block_device.rs
@@ -0,0 +1,75 @@
+use alloc::sync::Arc;
+use rsext4::{BLOCK_SIZE, BlockDevice, Ext4Error, Ext4Result, Ext4Timestamp};
+use spin::Mutex;
+use virtio_drivers::{
+    Hal,
+    device::blk::{SECTOR_SIZE, VirtIOBlk},
+    transport::Transport,
+};
+
+pub struct Ext4BlockDevice<H: Hal, T: Transport> {
+    block_device: Arc<Mutex<VirtIOBlk<H, T>>>,
+    sectors_per_block: usize,
+}
+
+impl<H: Hal, T: Transport> Ext4BlockDevice<H, T> {
+    pub fn new(block_device: Arc<Mutex<VirtIOBlk<H, T>>>) -> Self {
+        return Self {
+            block_device,
+            sectors_per_block: BLOCK_SIZE / SECTOR_SIZE,
+        };
+    }
+}
+
+impl<H: Hal, T: Transport> BlockDevice for Ext4BlockDevice<H, T> {
+    fn write(&mut self, buffer: &[u8], block_id: rsext4::bmalloc::AbsoluteBN, _count: u32) -> rsext4::Ext4Result<()> {
+        let mut block_device = self.block_device.lock();
+        if let Err(_) = block_device.write_blocks(block_id.as_usize()? * self.sectors_per_block, buffer) {
+            return Ext4Result::Err(Ext4Error::io());
+        }
+        Ok(())
+    }
+
+    fn read(&mut self, buffer: &mut [u8], block_id: rsext4::bmalloc::AbsoluteBN, _count: u32) -> rsext4::Ext4Result<()> {
+        let mut block_device = self.block_device.lock();
+        if let Err(_) = block_device.read_blocks(block_id.as_usize()? * self.sectors_per_block, buffer) {
+            return Ext4Result::Err(Ext4Error::io());
+        }
+        Ok(())
+    }
+
+    fn open(&mut self) -> rsext4::Ext4Result<()> {
+        Ok(())
+    }
+
+    fn close(&mut self) -> rsext4::Ext4Result<()> {
+        Ok(())
+    }
+
+    fn total_blocks(&self) -> u64 {
+        return self.block_device.lock().capacity() / self.sectors_per_block as u64;
+    }
+
+    fn current_time(&self) -> rsext4::Ext4Result<rsext4::Ext4Timestamp> {
+        return Ok(Ext4Timestamp::new(0, 0));
+    }
+
+    fn block_size(&self) -> u32 {
+        return SECTOR_SIZE as u32;
+    }
+
+    fn flush(&mut self) -> rsext4::Ext4Result<()> {
+        if let Err(_) = self.block_device.lock().flush() {
+            return Ext4Result::Err(Ext4Error::io());
+        }
+        Ok(())
+    }
+
+    fn is_open(&self) -> bool {
+        true
+    }
+
+    fn is_readonly(&self) -> bool {
+        false
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 #![no_main]
 #![no_std]
 
+use crate::libraries::console_utils::println;
+
 extern crate alloc;
 
 mod context;
@@ -16,6 +18,7 @@ mod thread;
 mod timer_interrupt;
 mod trap_handler;
 mod virtio_hal;
+mod ext4_block_device;
 
 unsafe extern "C" {
     static _kernel_end: u8;
@@ -54,6 +57,14 @@ extern "C" fn kmain(hart_id: usize, device_tree_binary_ptr: usize) -> ! {
     };
 
     devices::load_virtio_devices(&device_tree);
+
+    if let Some(virtio_blk) = devices::BLOCK_DEVICES.lock().get_mut(0) {
+        let ext4_blk = ext4_block_device::Ext4BlockDevice::new(virtio_blk.clone());
+        let ref mut block_dev = rsext4::Jbd2Dev::initial_jbd2dev(0, ext4_blk, true);
+        let mut fs = rsext4::Ext4FileSystem::mount(block_dev).unwrap();
+        let root = fs.get_root(block_dev).unwrap();
+        println!("{:x}",root.i_flags);
+    }
 
     thread::create_thread(programs::shell::new as *const u8 as usize, false, 16384, &[]);
 
